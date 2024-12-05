286e47ac7ef6c4359fe193058f1731492e8824e0
medici <medici@medicis-MacBook-Pro.local>
2024-12-05 18:28:38 +0700

    initial-commit

---
+++ src/components/NextApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NextApp = () => {
+  return (
+    <div>
+      <h2>Here Component from Next.js</h2>
+      <p>Next.js is available in Astro!</p>
+    </div>
+  );
+};
+
+export default NextApp;
+
