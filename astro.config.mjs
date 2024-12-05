f4d66b4fbdbc12c433ea757b23aa102c874f0622
medici <medici@medicis-MacBook-Pro.local>
2024-12-06 00:28:18 +0700

    fix-path-again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://0xwrpj.github.io',
-  	base: '0xwrpj.github.io',
+  	base: process.env.NODE_ENV === 'production' ? '/0xwrpj.github.io' : '/',
 	integrations: [react()],
 });
